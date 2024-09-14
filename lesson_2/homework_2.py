21a6699cde5d5573a7a8f24a6d99088886d41f1b
Сидоров Илья <119235052+profalse@users.noreply.github.com>
2024-09-14 18:58:13 +0300

    Update homework_2.py

--- lesson_2/homework_2.py
+++ lesson_2/homework_2.py
@@ -1,4 +1,30 @@
 # Number 1
-s = '12345'
-str_1 = s[1] + s[0] + s[3] + s[2] + s[4]
-print(str_1)
+# A = list(map(int, input().split()))
+# A_1 = A[1] + A[0] + A[3] + A[2] + A[4]
+# print(A_1)
+
+# Number 2
+# B = list(map(int, input().split()))
+# print(B.insert(0, B.pop()), [i for i in B])
+
+# Numder 3
+# C = list(map(int, input().split()))
+# for i in C:
+#     if C.count(i) == 1:
+#         print(i)
+
+# Number 4
+# E = list(map(int, input().split()))
+# maximum = E.count(E[0])
+# for i in E:
+#     if E.count(i) > maximum:
+#         maximum = E.count(i)
+# print(E[i])
+
+# Number 5
+# n, s = input().split()
+# n = int(n)
+# res = list()
+# for i in range(0, len(s), n):
+#     res += reversed(s[i:i+n])
+# print(''.join(res))
